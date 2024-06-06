77e9b11263edaddf072ab567ff4c8cbaee8ed137
bryansine <bryansine1738@gmail.com>
2024-06-07 02:29:43 +0300

    Updates

--- App/base/views.py
+++ App/base/views.py
@@ -4,18 +4,18 @@ from .forms import PetitionSignupForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-@login_required
-def petition_signup(request):
-    if request.method == 'POST':
-        form = PetitionSignupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Thank you for signing the petition!')
-            return redirect('home')
-    else:
-        form = PetitionSignupForm()
+# @login_required
+# def petition_signup(request):
+#     if request.method == 'POST':
+#         form = PetitionSignupForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Thank you for signing the petition!')
+#             return redirect('home')
+#     else:
+#         form = PetitionSignupForm()
     
-    return render(request, 'events/petition_signup.html', {'form': form})
+#     return render(request, 'events/petition_signup.html', {'form': form})
 
 @login_required
 def petition_signup(request):
